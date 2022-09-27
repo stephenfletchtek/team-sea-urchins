d28956167924adc6985a82853c4c8fd21f23ebb3
Harry Fox <49591810+hfox1@users.noreply.github.com>
2022-09-27 12:37:48 +0100

    player - jumping nearly done

--- script.js
+++ script.js
@@ -8,13 +8,85 @@ window.addEventListener('load', function () {
 		constructor() {
 			//applies event listeners, keeps track of keys pressed
 			this.keys = [];
-			window.addEventListener('keydown', function (e) {
-				console.log(e);
+			window.addEventListener('keydown', (e) => {
+				console.log(e.key);
+				if (
+					(e.key === 'ArrowDown' ||
+						e.key === 'ArrowUp' ||
+						e.key === 'ArrowLeft' ||
+						e.key === 'ArrowRight') &&
+					this.keys.indexOf(e.key) === -1
+				) {
+					this.keys.push(e.key);
+				}
+			});
+			window.addEventListener('keyup', (e) => {
+				if (
+					e.key === 'ArrowDown' ||
+					e.key === 'ArrowUp' ||
+					e.key === 'ArrowLeft' ||
+					e.key === 'ArrowRight'
+				) {
+					this.keys.splice(this.keys.indexOf(e.key), 1);
+				}
 			});
 		}
 	}
 
-	class Player {}
+	class Player {
+		constructor(gameWidth, gameHeight) {
+			this.gameWidth = gameWidth;
+			this.gameHeight = gameHeight;
+			this.width = 200;
+			this.height = 200;
+			this.x = 0;
+			this.y = this.gameHeight - this.height; //(this.gameHeight - this.height)/2 for sub;
+			this.image = document.getElementById('playerImage');
+      this.frameX = 1; //these move along the spritesheet to grab whichever animation frame  
+      this.frameY = 1;
+      this.speed = 0;
+      this.vy = 0; // y-velocity (remember negative sign means go up)
+      this.weight = 1;
+
+		}
+		draw(context) {
+			context.fillStyle = 'white';
+			context.fillRect(this.x, this.y, this.width, this.height);
+			context.drawImage(
+			  this.image, 0, 0, this.width, this.height, this.x, this.y, this.width, this.height
+			); //s_ = coords in spritesheet
+		}
+		update(input) {
+      if (input.keys.indexOf('ArrowRight') > -1 ){
+        this.speed = 5;
+      } else if (input.keys.indexOf('ArrowLeft') > -1 ){
+          this.speed = -5; //should this be -3 to make the left/right movement the same 
+      } else if (input.keys.indexOf('ArrowUp') > -1 ){
+          this.vy = -5
+      } 
+      
+      else {
+        this.speed = 0;
+      }
+      // set limits so you can't go off screen
+          // horizontal movement
+			this.x += this.speed;
+      if (this.x < 0) this.x = 0;
+      else if (this.x > this.gameWidth - this.width) this.x = this.gameWidth - this.width;
+      // vertical movemet 
+      if (!this.onGround()) {
+        this.vy += this.weight; // acceleration due to gravity
+      }
+      else {
+        this.vy = 0;
+      }
+      }
+		}  
+
+    onGround(){
+      return this.y >= this.gameHeight - this.height;
+    }
+	}
 
 	class Background {}
 
@@ -23,7 +95,17 @@ window.addEventListener('load', function () {
 	const handleEnemies = () => {};
 
 	const diplayStatusText = () => {};
-	const animate = () => {};
-});
 
-const InputHandler = new InputHandler();
+	const input = new InputHandler();
+	const player = new Player(canvas.width, canvas.height);
+	player.draw(ctx);
+	player.update();
+
+	const animate = () => {
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		player.draw(ctx);
+		player.update(input);
+		this.requestAnimationFrame(animate);
+	};
+	animate();
+});
