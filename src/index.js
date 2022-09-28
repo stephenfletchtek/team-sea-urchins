37e6f9595a43f045f58cdcae9ead335235f4663d
sophie <soph_lew@hotmail.co.uk>
2022-09-28 17:58:49 +0100

    updated test for contains canvas

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ function initScene(){};
 
 function preloadScene(){
   this.load.svg('background', 'assets/background/whole-background.svg', { width: 1920, height: 1080 });
-  this.load.svg('player', 'assets/players/player-fish.svg', { width: 150, height: 150 })
+  this.load.svg('player', 'assets/players/player-fish.svg', { width: 150, height: 150 });
 };
 
 function createScene(){
@@ -11,14 +11,15 @@ function createScene(){
   resize();
   const background = this.add.image(0,0, 'background').setOrigin(0, 0);
 
-  let scaleX = this.cameras.main.width / background.width
-  let scaleY = this.cameras.main.height / background.height
-  let scale = Math.min(scaleX, scaleY)
-  background.setScale(scale).setScrollFactor(0)
+  let scaleX = this.cameras.main.width / background.width;
+  let scaleY = this.cameras.main.height / background.height;
+  let scale = Math.min(scaleX, scaleY);
+  background.setScale(scale).setScrollFactor(0);
 
   // player
   const screenCenterY = this.cameras.main.height / 2;
   const player = this.add.image(150, screenCenterY, 'player').setOrigin(0, 0.5);
+  console.log(game.canvas.image)
 };
 
 function resize() {
@@ -34,7 +35,10 @@ function resize() {
   }
 }
 
-function updateScene(){};
+function updateScene(){
+  // player moves up or down
+
+};
 
  
  const config = {
