b7cf8662d1b82ee4ce07c2563e453cae34118f03
sophie <soph_lew@hotmail.co.uk>
2022-09-28 16:48:30 +0100

    Added and sized a background

--- src/index.js
+++ src/index.js
@@ -1,18 +1,44 @@
 function initScene(){};
 
-function preloadScene(){};
+function preloadScene(){
+  this.load.svg('background', 'assets/background/whole-background.svg', { width: 1920, height: 1080 });
+  // this.load.image('player', 'assets/players/player-fish.svg')
+};
 
-function createScene(){};
+function createScene(){
+  window.addEventListener('resize', resize);
+  resize();
+  const background = this.add.image(0,0, 'background').setOrigin(0, 0);
+  
+
+  let scaleX = this.cameras.main.width / background.width
+  let scaleY = this.cameras.main.height / background.height
+  let scale = Math.min(scaleX, scaleY)
+  background.setScale(scale).setScrollFactor(0)
+
+};
+
+function resize() {
+  let canvas = game.canvas, width = window.innerWidth, height = window.innerHeight;
+  let wratio = width / height, ratio = canvas.width / canvas.height;
+
+  if (wratio < ratio) {
+      canvas.style.width = width + "px";
+      canvas.style.height = (width / ratio) + "px";
+  } else {
+      canvas.style.width = (height * ratio) + "px";
+      canvas.style.height = height + "px";
+  }
+}
 
 function updateScene(){};
 
  
- const PhaserConfig = {
+ const config = {
   type: Phaser.AUTO,
   parent: "game",
   width: 1280,
   height: 720,
-  backgroundColor: "#5DACD8",
   scene: {
     init: initScene,
     preload: preloadScene,
@@ -21,4 +47,4 @@ function updateScene(){};
   },
 };
 
-const game = new Phaser.Game(PhaserConfig);
+const game = new Phaser.Game(config);
