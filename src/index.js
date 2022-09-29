4e9249d95dff6245c484566d49bce96b1ceae2a1
naomischlosser <naomi_schlosser@hotmail.com>
2022-09-29 11:05:59 +0100

    some furter styling

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ function initScene(){};
 
 function preloadScene(){
   this.load.svg('background', 'assets/background/whole-background.svg', { width: 1920, height: 1080 });
-  // this.load.svg('player', 'assets/players/player-fish.svg', { width: 150, height: 150 });
-  this.load.svg('player', 'assets/players/player-fish.svg', { width: 300, height: 300 });
+  this.load.svg('player', 'assets/players/player-fish.svg', { width: 200, height: 200 });
 };
 
 function createScene(){
@@ -11,16 +10,14 @@ function createScene(){
   window.addEventListener('resize', resize);
   resize();
   const background = this.add.image(0,0, 'background').setOrigin(0, 0);
-
-  let scaleX = this.cameras.main.width / background.width;
-  let scaleY = this.cameras.main.height / background.height;
-  let scale = Math.min(scaleX, scaleY);
-  background.setScale(scale).setScrollFactor(0);
+  // let scaleX = this.cameras.main.width / background.width;
+  // let scaleY = this.cameras.main.height / background.height;
+  // let scale = Math.min(scaleX, scaleY);
+  // background.setScale(scale).setScrollFactor(0);
 
   // player
   const screenCenterY = this.cameras.main.height / 2;
-  const player = this.add.image(150, screenCenterY, 'player').setOrigin(0, 0.5);
-  player.setScale(0.7).setScrollFactor(0);
+  const player = this.add.image(300, screenCenterY, 'player');
 };
 
 function resize() {
