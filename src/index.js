d6e1d822553a187753c5e708a309a1ef02774c5d
naomischlosser <naomi_schlosser@hotmail.com>
2022-09-29 16:58:47 +0100

    user can swipe up and down to make the fish move

--- src/index.js
+++ src/index.js
@@ -23,9 +23,6 @@ const game = new Phaser.Game(config);
 function initScene(){}; 
 
 function preloadScene() {
-  // swipe player
-
-
   // load background
   this.load.svg('background', 'assets/background/whole-background.svg', { width: 1920, height: 1080 });
   
@@ -37,9 +34,6 @@ function preloadScene() {
 };
 
 function createScene() {
-  // swipe player
-
-
   // background
   window.addEventListener('resize', resize);
   resize();
@@ -52,7 +46,6 @@ function createScene() {
   // player
   const screenCenterY = this.cameras.main.height / 2;
   this.player = this.physics.add.sprite(300, screenCenterY, 'player');
-  this.player.setScale(0.7).setScrollFactor(0);
 
   // obstacle
   for(const obstacle of obstacleArray){
@@ -63,22 +56,79 @@ function createScene() {
 
 function updateScene(){
   // player moves up or down
-  cursors = this.input.keyboard.createCursorKeys()
-  let velocity = 250
+  cursors = this.input.keyboard.createCursorKeys();
+  // setting the speed that the player moves
+  let velocity = 300;
+
+  // limit of where the player can go
+  let upperLim = this.player.height / 2;
+  let lowerLim = game.canvas.height - upperLim;
+
+  // player responds to the up and down keys being clicked, which can intercept a swipe
   if (cursors.down.isDown) {
-    this.player.setVelocityY(velocity)
+    swipeDirection = ""
+    if (this.player.y < lowerLim) {
+      this.player.setVelocityY(velocity)
+    } else {
+      this.player.setVelocity(0)
+    }
   } else if (cursors.up.isDown) {
-    this.player.setVelocityY(-velocity)
-  } else if (cursors.down.issUp) {
+    swipeDirection = ""
+    if (this.player.y > upperLim){
+      this.player.setVelocityY(-velocity)
+    } else {
+      this.player.setVelocity(0)
+    }
+  } else if (cursors.down.isUp) {
     this.player.setVelocityY(0)
   } else if (cursors.up.isUp) {
     this.player.setVelocityY(0)
   }
   
-  };
+  // mimick swiping
+  // if not 'down' and clicking is true
+  if(!this.input.activePointer.isDown && isClicking == true) {
+
+    // if the movement is more than 50
+    if(Math.abs(this.input.activePointer.upY - this.input.activePointer.downY) >= 50) {
+        // up swipe
+        if(this.input.activePointer.upY < this.input.activePointer.downY) {
+            swipeDirection = "up";
+        // down swipe
+        } else if(this.input.activePointer.upY > this.input.activePointer.downY) {
+            swipeDirection = "down";
+        }
+    }
+    isClicking = false;
+  } else if(this.input.activePointer.isDown && isClicking == false) {
+      isClicking = true;
+  }
+
+  // player swipes down
+  if(swipeDirection == "down") {
+    if (this.player.y < lowerLim) {
+      this.player.setVelocityY(velocity)
+    } else {
+      this.player.setVelocity(0)
+      swipeDirection = ""
+    }
+  }
+
+  // player swipes up
+  if(swipeDirection == "up") {
+    if (this.player.y > upperLim){
+      this.player.setVelocityY(-velocity)
+    } else {
+      this.player.setVelocity(0)
+      swipeDirection = ""
+    }
+  }
+};
 
 
 // Additional code
+let isClicking = false;
+let swipeDirection;
 
 function resize() {
   let canvas = game.canvas, width = window.innerWidth, height = window.innerHeight;
