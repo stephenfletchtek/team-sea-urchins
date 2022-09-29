5b705d17eb34ec8f07f29981002fd9ff66cb39ec
naomischlosser <naomi_schlosser@hotmail.com>
2022-09-29 10:46:59 +0100

    styling and sizing

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ function initScene(){};
 
 function preloadScene(){
   this.load.svg('background', 'assets/background/whole-background.svg', { width: 1920, height: 1080 });
-  this.load.svg('player', 'assets/players/player-fish.svg', { width: 150, height: 150 });
+  // this.load.svg('player', 'assets/players/player-fish.svg', { width: 150, height: 150 });
+  this.load.svg('player', 'assets/players/player-fish.svg', { width: 300, height: 300 });
 };
 
 function createScene(){
@@ -19,7 +20,7 @@ function createScene(){
   // player
   const screenCenterY = this.cameras.main.height / 2;
   const player = this.add.image(150, screenCenterY, 'player').setOrigin(0, 0.5);
-  console.log(game.canvas.image)
+  player.setScale(0.7).setScrollFactor(0);
 };
 
 function resize() {
@@ -44,8 +45,8 @@ function updateScene(){
  const config = {
   type: Phaser.AUTO,
   parent: "game",
-  width: 1280,
-  height: 720,
+  width: 1920,
+  height: 1080,
   scene: {
     init: initScene,
     preload: preloadScene,
