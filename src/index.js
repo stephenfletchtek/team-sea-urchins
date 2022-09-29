e03a65d9379994663d0ba450d9a7c82faef3162e
ruben wilson <rwilson_connell@yahoo.com>
2022-09-29 17:33:33 +0100

    collison nets for objects, and collison physics for sprites

--- src/index.js
+++ src/index.js
@@ -23,14 +23,14 @@ const game = new Phaser.Game(config);
 function initScene(){};
 
 function preloadScene(){
+  //load in physics file 
   this.load.json('physics', 'assets/physics.json')
 
   this.load.svg('background', 'assets/background/whole-background.svg', { width: 1920, height: 1080 });
   this.load.image('player', 'assets/players/player-fish.png');
  
   // load obstacles 
-  this.load.svg('rockObstacle', 'assets/obstacles/rock.svg');
- 
+  this.load.svg('rockObstacle', 'assets/obstacles/obstacle-rock.svg');
   this.load.image('obstacle-ship', 'assets/obstacles/obstacle-ship-wreck.png');
 };
 
@@ -59,20 +59,47 @@ function createScene(){
   const screenCenterY = this.cameras.main.height / 2;
   
   //load in json physics file
-  spritePhysics = this.cache.json.get("physics");
+  physics = this.cache.json.get("physics");
 
-  player = this.matter.add.sprite(150, screenCenterY, "player", null, { shape: spritePhysics.fish });
+  // create player sprite
+  player = this.matter.add.sprite(150, screenCenterY, "player", null, { shape: physics.fish });
   player.setScale(0.7).setScrollFactor(0);
 
+  // obstacles = this.matter.add.sprite(600, 700, 'rockObstacle', null, {shape: physics.rock});
+  // obstacles.setVelocityX(-15);
   
-  // obstacle
+  
+  // generate obstacles
   for(const obstacle of obstacleArray){
     setTimeout(()=>{
-                    const obstacles = this.matter.add.sprite(obstacle.x, obstacle.y, obstacle.name, null,{ shape: spritePhysics[`${obstacle.outline}`] });
-                    obstacles.setFriction(0);
-                    obstacles.setVelocityX(-15);
+                    const obstacles = this.matter.add.sprite(obstacle.x, obstacle.y, obstacle.name, null,{ shape: physics[`${obstacle.outline}`] });
+                    obstacles.setVelocityX(-10);
                     obstacles.setMass(200); }, obstacle.time)
   }
+  
+// animation code example code here if https://www.thepolyglotdeveloper.com/2020/08/use-matterjs-physics-sprite-collisions-phaser-game/
+// this.matter.world.on('collisionactive', listener).event.pairs
+
+// check out collisionFilter.group to be able to control colliding classes
+
+// this.matter.world.on("collisionstart", (event, bodyA, bodyB) => {
+//   console.log("listening to event");
+//   console.log({a: bodyA, b: bodyB})
+//   if(bodyA.parent.label == "player-fish"){
+//     console.log("1");
+//     if(bodyA.bounds.max.x < 119){
+//       console.log(bodyA); 
+//       this.matter.pause();
+//     }
+//     }else if(bodyB.parent.label == "player-fish"){
+//       console.log("2");
+//         console.log(bodyB); 
+//         this.matter.pause();
+//       }
+      
+//     });
+
+
 
 };
 
@@ -88,6 +115,7 @@ function resize() {
       canvas.style.width = (height * ratio) + "px";
       canvas.style.height = height + "px";
   }
+
 }
 
 
