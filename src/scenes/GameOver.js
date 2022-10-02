b4beb63267a3e7059d50cea5b9ae8a4bb3ad2a56
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-10-02 17:28:16 +0100

    game over updated

--- src/scenes/GameOver.js
+++ src/scenes/GameOver.js
@@ -40,7 +40,7 @@ export default class GameOver extends Phaser.Scene {
 		let startAgain = this.add.text(width, height + 100, `Start again`, {
 			font: "bold 30px Arial",
 			fill: "#fff",
-		}).setOrigin(0.5);
+		}).setOrigin(0.5).setDepth(1);
 		startAgain.setInteractive();
 		startAgain.on("pointerup", () => {
 			console.log("lets play again!")
