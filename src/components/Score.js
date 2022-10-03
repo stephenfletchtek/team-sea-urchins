2544014987c7d1a432cbedb201594da8cf68a883
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-10-03 21:02:10 +0100

    improved score code

--- src/components/Score.js
+++ src/components/Score.js
@@ -1,23 +1,23 @@
 export default class Score {
   constructor(scene){
     this.scene = scene
-    // initialise display score
-    this.displayScore = 0;
   }
 
   createScore(){
+    this.tick = 0;
+    this.score = 0;
     const style = { font: "bold 50px Arial", fill: "#000000" };
     this.text = this.scene.add.text(this.scene.cameras.main.width / 2, 0, ``, style)
-
-    this.score = 0;
   }
 
   updateScore(){
-    this.displayScore += 1;
-    this.scene.time.addEvent({ delay: 1000, repeat: 1000000 }, (this.score += 1 * this.scene.gameSpeed/10));
-    if(this.displayScore % 7 == 0){
-      console.log(this.score);
-      this.text.setText(`${this.score}`);
+    this.tick += 1;
+    // score increment linked to gamespeed
+    this.score += 1 * this.scene.gameSpeed / 10
+    
+    // print score every 7th frame
+    if (this.tick % 7 == 0){
+      this.text.setText(Math.floor(this.score));
     }
   }
 }
