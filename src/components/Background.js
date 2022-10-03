b4680b0e5353105996210dffb44c6ec3648258a4
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-10-03 22:52:51 +0100

    remove unnecessary code from background.js

--- src/components/Background.js
+++ src/components/Background.js
@@ -4,23 +4,12 @@ export default class Background {
   }
 
   createMovingBackground() {
-    // old
-    // this.scene.add.image(0, 0, 'background').setOrigin(0, 0);
-
-    const background = this.scene.add.tileSprite(0, 0, 1920, 1080, 'background').setOrigin(0,0);
-    const scaleX = this.scene.cameras.main.width / background.width;
-    const scaleY = this.scene.cameras.main.height / background.height;
-    const scale = Math.min(scaleX, scaleY);
-
     this.ground6 = this.scene.add.tileSprite(0, 0, 1920, 1080, 'background6').setOrigin(0,0);
     this.ground5 = this.scene.add.tileSprite(0, 0, 1920, 1080, 'background5').setOrigin(0,0);
     this.ground4 = this.scene.add.tileSprite(0, 0, 1920, 1080, 'background4').setOrigin(0,0);
     // this.ground3 = this.scene.add.tileSprite(0, 0, 1920, 1080, 'background3').setOrigin(0,0);
     this.ground2 = this.scene.add.tileSprite(0, 0, 1920, 1080, 'background2').setOrigin(0,0);
     this.ground1 = this.scene.add.tileSprite(0, 0, 1920, 1080, 'background1').setOrigin(0,0);
-
-    this.background = background
-    this.background.setScale(scale)
   }
 
   updateMovingBackground(){
