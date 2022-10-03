81e8df0e80a4be128ea4fda78d8d8b648378e8ba
naomischlosser <naomi_schlosser@hotmail.com>
2022-10-03 17:22:24 +0100

    refactor obstacles.js

--- src/components/Obstacles.js
+++ src/components/Obstacles.js
@@ -4,32 +4,17 @@ export default class Obstacles {
   }
 
   createObstacles() {
-    // load in physics files
-    let physics = this.scene.cache.json.get("obstacles-physics");
-
-    // add to group
-    this.obstacles = this.scene.add.group()
-    this.ships = this.scene.add.group()
-    this.sharks = this.scene.add.group()
-
-    // add 6 of each obstacle into their respective groups
-    // make sure you don't get more obstacles on the the screen than there are in the group
-    for (let i = 0; i < 5; i++) {
-      this.obstacles.add(makeImage(this.scene, 'rockObstacle', physics.rock)).setVisible(false);
-      this.ships.add(makeImage(this.scene, 'shipObstacle', physics.ship)).setVisible(false);
-      this.sharks.add(makeImage(this.scene, 'sharkObstacle', physics.shark)).setVisible(false)
-    }
+    // create and populate obstacle groups
+    this.#createGroups()
+    this.#populateGroups()
 
     // add sharks
     this.scene.time.addEvent({
       delay: 5000,
       loop: true,
       callback: () => {
-        let obstaclePosition = Math.floor(Math.random() * 375) + 125
-        this.sharks.get(this.scene.cameras.main.width, obstaclePosition)
-          .setActive(true)
-          .setVisible(true)
-          .setScale(0.5)
+        let obstacleY = Math.floor(Math.random() * 375) + 125
+        this.#obstacleCallback(this.sharks, obstacleY)
       }
     })
 
@@ -38,41 +23,61 @@ export default class Obstacles {
       delay: 10000,
       loop: true,
       callback: () => {
-        if (Math.round(Math.random()) == 0){
-          this.ships.get(this.scene.cameras.main.width, 970)
-          .setActive(true)
-          .setVisible(true)
-          .setScale(0.5)
+        if (Math.round(Math.random()) == 0) {
+          this.#obstacleCallback(this.rocks, 970)
         } else {
-          this.obstacles.get(this.scene.cameras.main.width, 970)
-          .setActive(true)
-          .setVisible(true)
-          .setScale(0.5)
+          this.#obstacleCallback(this.ships, 970)
         }
       }
     })
+  }
 
-    function makeImage(scene, image, physics) {
-      return scene.matter.add.image(-200, -200, image, null, { shape: physics });
+  updateObstacles() {
+    this.#controlObstacle(this.rocks, -3)
+    this.#controlObstacle(this.ships, -3)
+    this.#controlObstacle(this.sharks, -8)
+  }
+
+  #createGroups() {
+    this.rocks = this.scene.add.group();
+    this.ships = this.scene.add.group();
+    this.sharks = this.scene.add.group();
+  }
+
+  #populateGroups(){
+    // load in physics files
+    const physics = this.scene.cache.json.get("obstacles-physics");
+    
+    // add 6 of each obstacle into their respective groups
+    // make sure you don't get more obstacles on the the screen than there are in the group
+    for (let i = 0; i < 5; i++) {
+      this.rocks.add(this.#makeImage(this.scene, 'rockObstacle', physics.rock)).setVisible(false);
+      this.ships.add(this.#makeImage(this.scene, 'shipObstacle', physics.ship)).setVisible(false);
+      this.sharks.add(this.#makeImage(this.scene, 'sharkObstacle', physics.shark)).setVisible(false)
     }
   }
 
-  updateObstacles(){
-    controlObstacle(this.obstacles, -3)
-    controlObstacle(this.ships, -3)
-    controlObstacle(this.sharks, -8)
+  #makeImage(scene, image, physics) {
+    return scene.matter.add.image(-200, -200, image, null, { shape: physics });
+  }
 
-    function controlObstacle(group, speed){
-      group.incX(speed);
-      group.getChildren().forEach(obstacle => {
-        obstacle.setAngle(0);
-        obstacle.setVelocityX(0);
-        obstacle.setVelocityY(0);
+  #obstacleCallback(obstacles, obstacleY) {
+    obstacles.get(this.scene.cameras.main.width, obstacleY)
+      .setActive(true)
+      .setVisible(true)
+      .setScale(0.5)
+  }
 
-        if (obstacle.active && obstacle.x < -200) {
-          group.killAndHide(obstacle)
-        }
-      })    
-    }
+  #controlObstacle(group, speed) {
+    group.incX(speed);
+    group.getChildren().forEach(obstacle => {
+      obstacle.setAngle(0);
+      obstacle.setVelocityX(0);
+      obstacle.setVelocityY(0);
+
+      if (obstacle.active && obstacle.x < -200) {
+        group.killAndHide(obstacle)
+      }
+    })    
   }
 }
