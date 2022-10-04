6e71d50ae7d306ddf36725afa8678a9248081787
Harry Fox <49591810+hfox1@users.noreply.github.com>
2022-10-04 12:29:46 +0100

    pre-start scene working

--- src/scenes/GameStart.js
+++ src/scenes/GameStart.js
@@ -3,20 +3,38 @@ import BaseGame from './BaseGame.js';
 export default class GameStart extends BaseGame {
   constructor() {
     super('game-start');
+    this.music;
   }
 
-  create() {
+  create(data) {
+    this.music = data.music;
+
     // background
-    this.setStaticBackground()
+    this.setStaticBackground();
+
+    this.cameras.main.fadeIn(4000);
 
     // start game button at beginning
     const startButton = this.add.image(
       this.game.renderer.width / 2,
-      this.game.renderer.height / 2, 
-      "start-game-button")
+      this.game.renderer.height / 2,
+      'start-game-button',
+    );
+
+    this.tweens.add(
+      {
+        targets: startButton,
+        alpha: 1,
+        duration: 20000,
+        ease: 'Power2',
+      },
+      this,
+    );
+
     startButton.setDepth(1).setInteractive();
-    startButton.on("pointerup", () => {
-      this.scene.start("game-play")
-    })
+    startButton.on('pointerup', () => {
+      this.music.destroy();
+      this.scene.start('game-play');
+    });
   }
 }
---
+++ src/scenes/preStart.js
@@ -0,0 +1,65 @@
+import BaseGame from './BaseGame.js';
+
+export default class PreStart extends BaseGame {
+  constructor() {
+    super('pre-start');
+    this.music;
+  }
+
+  create() {
+    // music
+    this.music = this.sound.add('startGameMusic', { loop: true });
+
+    if (!this.sound.locked) {
+      // already unlocked so play
+      this.music.play();
+    } else {
+      // wait for 'unlocked' to fire and then play
+      this.sound.once(Phaser.Sound.Events.UNLOCKED, () => {
+        this.music.play();
+      });
+    }
+
+    this.cameras.main.fadeIn(3000);
+
+    const preGameButton = this.add
+      .image(this.game.renderer.width / 2, this.game.renderer.height / 2, 'pre-game-button')
+      .setScale(0.5)
+      .setInteractive();
+
+    let throb = this.tweens.add({
+      targets: preGameButton,
+      scale: 2,
+      yoyo: true,
+      repeat: -1,
+      duration: 800,
+      ease: 'sine',
+    });
+
+    // enterText.on('pointerup', () => {
+    preGameButton.on('pointerup', () => {
+      let tween = this.tweens.add(
+        {
+          targets: preGameButton,
+          alpha: 0,
+          duration: 500,
+          ease: 'Power2',
+        },
+        this,
+      );
+      tween.on('complete', () => {
+        // this.cameras.main.fadeOut(1500);
+        console.log('load StartGame scene');
+        this.scene.start('game-start', { music: this.music });
+      });
+    });
+  }
+}
+
+// var tween = this.tweens.add({
+//   targets: image,
+//   x: 500,
+//   ease: 'Power1',
+//   duration: 3000,
+// });
+// tween.on('complete', listener);
