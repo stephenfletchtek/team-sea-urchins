a2e748f93d1e2f66b8d3e9415109c3e1061f1f0d
Harry Fox <49591810+hfox1@users.noreply.github.com>
2022-10-04 14:19:58 +0100

    tinkering

--- src/scenes/BaseGame.js
+++ src/scenes/BaseGame.js
@@ -17,33 +17,39 @@ export default class BaseGame extends Phaser.Scene {
     this.load.audio('theme', 'assets/audio/GamePlay/themellq.mp3');
     this.load.audio('gameOverMusic', 'assets/audio/GameOver/gameover.mp3');
 
-    // load in physics files 
+    // load in physics files
     this.load.json('obstacles-physics', 'assets/obstacles/obstacles-physics.json');
     this.load.json('fish-physics', 'assets/players/fish-physics.json');
-  
+
     // load static background
-    this.load.svg('background', 'assets/background/whole-background.svg', { width: 1920, height: 1080 });
-  
+    this.load.svg('background', 'assets/background/whole-background.svg', {
+      width: 1920,
+      height: 1080,
+    });
+
     // load moving background
     this.load.svg('background0', 'assets/background/layer-0.svg', { width: 1920, height: 1080 });
     this.load.svg('background1', 'assets/background/layer-1.svg', { width: 1920, height: 1080 });
     this.load.svg('background2', 'assets/background/layer-2.svg', { width: 1920, height: 1080 });
     this.load.svg('background3', 'assets/background/layer-3.svg', { width: 1920, height: 1080 });
     this.load.svg('background4', 'assets/background/layer-4.svg', { width: 1920, height: 1080 });
-    this.load.svg('background5', 'assets/background/layer-5.svg', { width: 1920, height: 1080 }); 
-    this.load.svg('background6', 'assets/background/layer-6.svg', { width: 1920, height: 1080 }); 
+    this.load.svg('background5', 'assets/background/layer-5.svg', { width: 1920, height: 1080 });
+    this.load.svg('background6', 'assets/background/layer-6.svg', { width: 1920, height: 1080 });
 
+    // load player
+    this.load.atlas(
+      'player',
+      'assets/players/player-fish-spritesheet.png',
+      'assets/players/player-fish.json',
+    );
 
-    // load player    
-    this.load.atlas('player', 'assets/players/player-fish-spritesheet.png', 'assets/players/player-fish.json');
-  
     // load obstacles
     this.load.svg('rockObstacle', 'assets/obstacles/obstacle-rock.svg');
     this.load.image('shipObstacle', 'assets/obstacles/obstacle-ship-wreck.png');
-    this.load.image('sharkObstacle', 'assets/obstacles/obstacle-shark.png')
+    this.load.image('sharkObstacle', 'assets/obstacles/obstacle-shark.png');
   }
 
-  setStaticBackground(){
+  setStaticBackground() {
     // background
     this.add.image(0, 0, 'background').setOrigin(0, 0);
   }
