9f15ea6624e52abaa4ce2347757592d68de1035c
sophie <soph_lew@hotmail.co.uk>
2022-10-05 16:28:02 +0100

    bubbles and worms working

--- src/components/PowerUps.js
+++ src/components/PowerUps.js
@@ -19,18 +19,18 @@ export default class PowerUp {
 
     // add cans of worms
     if (this.tick % 50 == 0){
-      let YPos = Math.floor(Math.random() * 300) + 100;
-      this.#powerupCallback(this.worms, YPos, 0.5)
+      let XPosWorms = Math.floor(Math.random() * 1200) + 80;
+      this.#powerupCallback(this.worms, XPosWorms, -100, 0.5)
     }
 
     // add bubbles
     if (this.tick % 50 == 0){
-      let powerupX = Math.floor(Math.random() * 300) + 100;
-      this.#powerupCallback(this.bubbles, powerupX, 0.7)
+      let XPosBubbles = Math.floor(Math.random() * 1200) + 80;
+      this.#powerupCallback(this.bubbles, XPosBubbles, 1200, 0.4)
     }
 
-    this.#controlPowerUp(this.worms, -2.5 * this.scene.gameSpeed)
-    this.#controlPowerUp(this.bubbles, 0.5 * this.scene.gameSpeed)
+    this.#controlPowerUp(this.worms, 0.5 * this.scene.gameSpeed, -101, 1200)
+    this.#controlPowerUp(this.bubbles, -0.5 * this.scene.gameSpeed, -200, 1300)
   }
 
   #createGroups() {
@@ -41,15 +41,15 @@ export default class PowerUp {
   #populateGroups(){  
     // make sure you don't get more objects on the the screen than there are in the group!
     for (let i = 0; i < 1; i++) {
-      this.worms.add(this.scene.matter.add.image(-200, -200, 'wormPower', null, { shape: this.physics.worm })).setVisible(false);
+      this.worms.add(this.scene.matter.add.image(-200, -200, 'wormPower', null, { shape: this.physics.worm }));
       this.bubbles.add(this.#bubblesAnimation());
     }
   }
 
-  #powerupCallback(group, YPos, scale) {
+  #powerupCallback(group, XPos, YPos, scale) {
     // only add child if there is one available in the pool
     if (group.countActive() < group.getLength()){
-      group.get(this.scene.cameras.main.width, YPos)
+      group.get(XPos, YPos)
       .setActive(true)
       .setVisible(true)
       .setScale(scale)          
@@ -57,7 +57,7 @@ export default class PowerUp {
   }
 
   #bubblesAnimation() {
-    let bubbleImage = this.scene.matter.add.sprite(300, 300, 'bubblePower', null, { shape: this.physics.bubbles });
+    let bubbleImage = this.scene.matter.add.sprite(-200, -200, 'bubblePower', null, { shape: this.physics.bubbles });
 
     let bubblesMove = {
       key: 'bubbles-move',
@@ -77,14 +77,14 @@ export default class PowerUp {
     return bubbleImage;
   }
   
-  #controlPowerUp(group, speed) {
-    group.incX(speed);
+  #controlPowerUp(group, speed, upperLim, lowerLim) {
+    group.incY(speed);
     group.getChildren().forEach(powerup => {
       powerup.setAngle(0);
       powerup.setVelocityX(0);
       powerup.setVelocityY(0);
 
-      if (powerup.active && powerup.x < -200) {
+      if (powerup.active && (powerup.y < upperLim || powerup.y > lowerLim)) {
         group.killAndHide(powerup)
       }
     })    
